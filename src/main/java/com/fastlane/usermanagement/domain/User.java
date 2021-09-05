6f20be43a4cf7505da582a688743c8c77415f447
etff <wish2rich@gmail.com>
2021-09-05 20:22:56 +0900

    docs: api 기능설명 추가

--- src/main/java/com/fastlane/usermanagement/domain/User.java
+++ src/main/java/com/fastlane/usermanagement/domain/User.java
@@ -1,14 +1,21 @@
 package com.fastlane.usermanagement.domain;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.Objects;
 
 /**
  * 사용자.
  */
 @Getter
 public class User {
+
+    @ApiModelProperty(value = "PK", required = true)
     private Long id;
+
+    @ApiModelProperty(hidden = true)
     private String password;
 
     public User(String password) {
@@ -19,5 +26,26 @@ public class User {
     public User(Long id, String password) {
         this.id = id;
         this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(getId(), user.getId()) && Objects.equals(getPassword(), user.getPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getPassword());
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", password='" + password + '\'' +
+                '}';
     }
 }
--- src/main/java/com/fastlane/usermanagement/ui/UserController.java
+++ src/main/java/com/fastlane/usermanagement/ui/UserController.java
@@ -3,6 +3,8 @@ package com.fastlane.usermanagement.ui;
 import com.fastlane.usermanagement.application.UserService;
 import com.fastlane.usermanagement.dto.UserRequestDto;
 import com.fastlane.usermanagement.dto.UserResponseDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,32 +19,37 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.net.URI;
 
+@Api(tags = "사용자 API")
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
 @RestController
 public class UserController {
     private final UserService userService;
 
+    @ApiOperation(value = "사용자 가입")
     @PostMapping
     public ResponseEntity<UserResponseDto> registerUser(@RequestBody @Valid UserRequestDto dto) {
         UserResponseDto responseDto = userService.registerUser(dto.getPassword());
-        return ResponseEntity.created(URI.create("/users/" + responseDto.getUserId())).build();
+        return ResponseEntity.created(URI.create("/users/" + responseDto.getUserId())).body(responseDto);
     }
 
+    @ApiOperation(value = "사용자 조회")
     @GetMapping("/{userId}")
     public ResponseEntity<UserResponseDto> getUser(@PathVariable Long userId) {
         UserResponseDto responseDto = userService.getUser(userId);
         return ResponseEntity.ok(responseDto);
     }
 
+    @ApiOperation(value = "사용자 삭제")
     @DeleteMapping("/{userId}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
         userService.deleteUser(userId);
         return ResponseEntity.noContent().build();
     }
 
+    @ApiOperation(value = "사용자 비밀반호 수정")
     @PatchMapping("/{userId}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long userId, @RequestBody UserRequestDto dto) {
+    public ResponseEntity<Void> updateUserPassword(@PathVariable Long userId, @RequestBody UserRequestDto dto) {
         userService.updatePassword(userId, dto.getPassword());
         return ResponseEntity.ok().build();
     }
